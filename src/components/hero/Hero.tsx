9d3e0abd0b20fe603835b47f3315c230ebe7f0eb
dilshod <dilshodx516@gmail.com>
2025-01-18 11:13:41 +0500

    final

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,7 +1,36 @@
-
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <main className='w-full h-auto px-5'>
+      <div className='container h-[665px] hero p-20 max-md:p-10 max-sm:h-auto max-[450px]:p-5'>
+        <div className='flex flex-col gap-5'>
+          <h2 className='w-[577px] h-auto text-black font-extrabold text-6xl max-md:text-4xl max-md:w-auto max-[450px]:text-2xl'>
+            FIND CLOTHES THAT MATCHES YOUR STYLE
+          </h2>
+          <p className='w-[570px] h-auto text-black text-lg max-md:w-auto max-[450px]:text-sm'>
+            Browse through our diverse range of meticulously crafted garments,
+            designed to bring out your individuality and cater to your sense of
+            style.
+          </p>
+          <button className='w-52 h-14 rounded-full bg-primary duration-300 text-white hover:opacity-55 max-md:w-full'>
+            Shop Now
+          </button>
+          <div className='w-auto flex items-center  gap-10 mt-20 max-md:flex-wrap max-md:justify-center'>
+            <div className='flex flex-col max-sm:items-center'>
+              <p className="font-bold text-4xl">200+</p>
+              <span className="text-gray-600 text-xl">International Brands</span>
+            </div>
+            <div className='flex flex-col max-sm:items-center'>
+              <p className="font-bold text-4xl">2,000+</p>
+              <span className="text-gray-600 text-xl">High-Quality Products</span>
+            </div>
+            <div className='flex flex-col max-sm:items-center'>
+              <p className="font-bold text-4xl">30,000+</p>
+              <span className="text-gray-600 text-xl">Happy Customers</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- src/components/style/Style.tsx
+++ src/components/style/Style.tsx
@@ -1,6 +1,26 @@
 const Style = () => {
   return (
-    <div>Style</div>
+    <section className='w-full h-auto p-5'>
+      <div className='container bg-bgGray h-[870px] rounded-3xl py-20 px-20 flex flex-col gap-10 items-center max-lg:p-10 max-lg:justify-center max-lg:h-auto max-[450px]:p-5'>
+        <h2 className='text-5xl font-extrabold text-center text-primary max-lg:text-3xl max-sm:text-xl'>
+          BROWSE BY DRESS STYLE
+        </h2>
+        <div className='grid w-full'>
+          <div className='h-72 item1 rounded-2xl'>
+            <h3 className='text-4xl text-primary font-bold p-7 max-sm:text-2xl'>Casual</h3>
+          </div>
+          <div className='h-72 item2 rounded-2xl'>
+            <h3 className='text-4xl text-primary font-bold p-7 max-sm:text-2xl'>Formal</h3>
+          </div>
+          <div className='h-72 item3 rounded-2xl'>
+            <h3 className='text-4xl text-primary font-bold p-7 max-sm:text-2xl'>Party</h3>
+          </div>
+          <div className='h-72 item4 rounded-2xl'>
+            <h3 className='text-4xl text-primary font-bold p-7 max-sm:text-2xl'>Gym</h3>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
